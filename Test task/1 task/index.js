a96526c3c465d74276145ed9e26e6d1d39b89c8a
kiborg7075@gmail.com <kiborg7075@gmail.com>
2023-05-23 18:42:20 +0300

    edit

--- Test task/1 task/index.js
+++ Test task/1 task/index.js
@@ -4,16 +4,17 @@ const timerEl = document.querySelector('span');
 
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
+let stateOn=false
 const createTimerAnimator = () => {
   
   return (seconds) => {
-    
-    
+  
   let timer=  setInterval(()=>{
+      if(!stateOn){clearInterval(timer)}
       h=(seconds>3600)?Math.trunc((seconds/60)/60):0
       m=(seconds>60&&seconds<3600)?(Math.trunc(seconds/60)):Math.trunc((((seconds/60)/60)-Math.trunc((seconds/60)/60))*60)
       s=(seconds<60)?seconds:(Math.round(((seconds/60)-Math.trunc(seconds/60))*60))
-      timerEl.innerHTML=`${(String(h).length>1)?h:`0${h}`} : ${(String(m).length>1)?h:`0${m}`} : ${(String(s).length>1)?s:`0${s}`}`
+      timerEl.innerHTML=`${(String(h).length>1)?h:`0${h}`} : ${(String(m).length>1)?m:`0${m}`} : ${(String(s).length>1)?s:`0${s}`}`
       seconds--
       if(seconds<0){clearInterval(timer)}
     },1000)
@@ -31,7 +32,7 @@ inputEl.addEventListener('input', () => {
 
 buttonEl.addEventListener('click', () => {
   const seconds = Number(inputEl.value);
-
+  stateOn=(stateOn)?false:true
   animateTimer(seconds);
 
   inputEl.value = '';
